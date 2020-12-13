b842073d394448ea05e989f8cd5c4c998ec82088
Katy La Tour <klatour324@gmail.com>
2020-12-13 12:55:28 -0600

    Create tests for reviews greater than three or four

--- test/book_test.rb
+++ test/book_test.rb
@@ -44,39 +44,23 @@ class BookTest < Minitest::Test
 
      assert_equal [review_1, review_2], book.reviews
   end
+
+  def test_it_has_reviews_greater_than_3
+    book = Book.new({title: 'Practical Object Oriented Design in Ruby', author: 'Sandi Metz'})
+    review_1 = Review.new({name: "Aral Nuraduum", score: "3.5"})
+    review_2 = Review.new({name: "Aniz Arazak", score: "5.0"})
+
+
+    assert_equal [review_1, review_2], book.reviews_greater_than(3)
+  end
+
+  def test_it_has_reviews_greater_than_4
+    book = Book.new({title: 'Practical Object Oriented Design in Ruby', author: 'Sandi Metz'})
+    review_1 = Review.new({name: "Aral Nuraduum", score: "3.5"})
+    review_2 = Review.new({name: "Aniz Arazak", score: "5.0"})
+
+
+    assert_equal [review_2], book.reviews_greater_than(4)
+  end
 end
-  # Use TDD to create a `Book` class that responds to the following interaction pattern:
-  #
-  # * `#reviews` returns an array of review objects that belong to the book.
-  #
-  # ```ruby
-  # pry(main)> require './lib/review'
-  # # => true
-  #
-  #
-  # pry(main)> require './lib/book'
-  # # => true
-  #
-  # pry(main)> book.title
-  # # => 'Practical Object Oriented Design in Ruby'
-  #
-  # # => 'Sandi Metz'
-  #
-  # pry(main)> book.read?
-  # # => false
-  #
-  # pry(main)> book.read = true
-  #
-  # pry(main)> book.read?
-  # # => true
-  #
-  # # => #<Review:0x00007fd8273d21e0...>
-  #
-  # # => #<Review:0x0012fdabcd3d21e0...>
-  #
-  # pry(main)> book.reviews_greater_than(3)
-  # # => [#<Review:0x00007fd8273d21e0...>, #<Review:0x0012fdabcd3d21e0...>]
-  #
-  # pry(main)> book.reviews_greater_than(4)
-  # # => [#<Review:0x0012fdabcd3d21e0...>]
-  # ```
+  
