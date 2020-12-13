95633ea483eb0115fe2099ccd0c5afc2cfd65dba
DougWelchons <doug.welchons@gmail.com>
2020-12-13 11:18:11 -0700

    Add test to book test file

--- test/book_test.rb
+++ test/book_test.rb
@@ -36,4 +36,27 @@ class ReviewTest < Minitest::Test
 
     assert_equal true, book.read?
   end
+
+  def test_it_can_have_reviews
+    book = Book.new({title: 'Practical Object Oriented Design in Ruby', author: 'Sandi Metz'})
+    review_1 = Review.new({name: "Aral Nuraduum", score: "3.5"})
+    review_2 = Review.new({name: "Aniz Arazak", score: "5.0"})
+
+    book.add_review(review_1)
+    book.add_review(review_2)
+
+    assert_equal [review_1, review_2], book.reviews
+  end
+
+  def test_it_can_return_only_good_reviews
+    book = Book.new({title: 'Practical Object Oriented Design in Ruby', author: 'Sandi Metz'})
+    review_1 = Review.new({name: "Aral Nuraduum", score: "3.5"})
+    review_2 = Review.new({name: "Aniz Arazak", score: "5.0"})
+
+    book.add_review(review_1)
+    book.add_review(review_2)
+
+    assert_equal [review_1, review_2], book.reviews_greater_than(3)
+    assert_equal [review_2], book.reviews_greater_than(4)
+  end
 end
