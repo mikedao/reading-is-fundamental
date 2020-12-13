a6d9827fff4a3c148dcb13e062e375e16e571be6
Katy La Tour <klatour324@gmail.com>
2020-12-13 12:32:52 -0600

    Create test for reviews

--- test/book_test.rb
+++ test/book_test.rb
@@ -33,7 +33,17 @@ class BookTest < Minitest::Test
     assert_equal true, book.read?
   end
 
-  # pry(main)> book.author
+  def test_it_has_reviews
+    book = Book.new({title: 'Practical Object Oriented Design in Ruby', author: 'Sandi Metz'})
+    review_1 = Review.new({name: "Aral Nuraduum", score: "3.5"})
+    review_2 = Review.new({name: "Aniz Arazak", score: "5.0"})
+
+    book.add_review(review_1)
+
+    book.add_review(review_2)
+
+     assert_equal [review_1, review_2], book.reviews
+  end
 end
   # Use TDD to create a `Book` class that responds to the following interaction pattern:
   #
@@ -60,18 +70,9 @@ end
   # pry(main)> book.read?
   # # => true
   #
-  # pry(main)> review_1 = Review.new({name: "Aral Nuraduum", score: "3.5"})
   # # => #<Review:0x00007fd8273d21e0...>
   #
-  # pry(main)> review_2 = Review.new({name: "Aniz Arazak", score: "5.0"})
   # # => #<Review:0x0012fdabcd3d21e0...>
-  #
-  # pry(main)> book.add_review(review_1)
-  #
-  # pry(main)> book.add_review(review_2)
-  #
-  # pry(main)> book.reviews
-  # # => [#<Review:0x00007fd8273d21e0...>, #<Review:0x0012fdabcd3d21e0...>]
   #
   # pry(main)> book.reviews_greater_than(3)
   # # => [#<Review:0x00007fd8273d21e0...>, #<Review:0x0012fdabcd3d21e0...>]
