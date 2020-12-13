bf42f11a89eec94df839ac5988e4e69e7c54664d
Katy La Tour <klatour324@gmail.com>
2020-12-13 11:32:26 -0600

    Add reviewer first name method

--- lib/review.rb
+++ lib/review.rb
@@ -1,6 +1,11 @@
 class Review
+  attr_reader :reviewer
 
   def initialize(reviewer)
     @reviewer = reviewer
+  end
+
+  def reviewer_first_name
+    @reviewer.values.first.split[0].to_s
   end
 end
