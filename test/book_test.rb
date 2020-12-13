af2f22cee712ec9c9826e7a58905fe83a214bfa5
DougWelchons <doug.welchons@gmail.com>
2020-12-13 11:01:50 -0700

    add test to book test file

--- test/book_test.rb
+++ test/book_test.rb
@@ -10,4 +10,17 @@ class ReviewTest < Minitest::Test
 
     assert_instance_of Book, book
   end
+
+  def test_it_has_a_title
+    book = Book.new({title: 'Practical Object Oriented Design in Ruby', author: 'Sandi Metz'})
+
+    assert_equal 'Practical Object Oriented Design in Ruby', book.title
+  end
+
+  def test_it_has_an_author
+    book = Book.new({title: 'Practical Object Oriented Design in Ruby', author: 'Sandi Metz'})
+
+    assert_equal 'Sandi Metz', book.author
+  end
+
 end
